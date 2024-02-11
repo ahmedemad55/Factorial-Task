1e1e7e2258a2607ff955813777d2c74730100df7
Ahmed emad <ahmed.emad.23112003@gmail.com>
2024-02-11 13:46:32 +0200

    Factorial Task

---
+++ app/src/main/java/com/example/factorial_task/factorial.kt
@@ -0,0 +1,16 @@
+package com.example.factorial_task
+
+fun main(){
+    println("Enter number:")
+    val n = readLine()?.toIntOrNull()
+    var factorial :Int = 1
+    if (n!=null&& n > 0){
+        for(item in 1 ..n!!){
+            factorial *= item
+        }
+        println("the Factorial of $n is  = $factorial")
+    }else {
+        println("invalid")
+    }
+
+}
